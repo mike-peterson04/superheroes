fd115b3185d3b94b297836aee296a543986bdac1
Mike Peterson <mpeterson2004@gmail.com>
2021-05-04 14:07:26 -0600

    created placholder for heroes/index and have it displaying correctly

--- superheroes/heroes/models.py
+++ superheroes/heroes/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class Hero(models.Model):
-    name = models.CharField(nax_length=50)
-    identity = models.CharField(nax_length=50)
-    primary = models.CharField(nax_length=50)
-    secondary = models.CharField(nax_length=50)
-    catchphrase = models.CharField(nax_length=50)
+    hero_name = models.CharField(max_length=50)
+    secret_identity = models.CharField(max_length=50)
+    primary = models.CharField(max_length=50)
+    secondary = models.CharField(max_length=50, null=True)
+    catchphrase = models.CharField(max_length=50, null=True)
