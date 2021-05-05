5a8ad5de3c945956226b11f0009e60ba810512f1
Mike Peterson <mpeterson2004@gmail.com>
2021-05-05 14:13:30 -0600

    added bootstrap formatting to pages

--- superheroes/superheroes/urls.py
+++ superheroes/superheroes/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('heroes/', include('heroes.urls'))
+    path('heroes/', include('heroes.urls')),
+    path('', include('heroes.urls'))
 ]
